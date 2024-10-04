5810c9544c6ba7e3362209def50b315fd2d2cfb1
Gowtham Raju <gowthamraju239@gmail.com>
2024-10-04 20:08:08 +0530

    remove previous functions and wrtie new logic

--- app.js
+++ app.js
@@ -10,46 +10,67 @@ const pixelsWidth =document.querySelector(".width-pixels");
 const decrementButton = document.querySelector("#decrementButton");
 const incrementButton = document.querySelector("#incrementButton");
 
-// const pixels = document.querySelector("pixels");
 
 let pixels = parseInt(pixelsWidth.textContent);
-console.log(pixels);
 
-function warningMessage() {
-    if(pixels > 300) {
-        message.textContent = "Too big. Decrease the size of the image.";
-    } else if (pixels < 100) {
-        message.textContent = "Too small. Increase the size of the image.";
-    } else {
-        message.textContent = "";
-    }
-}
 
-function incrementAndDecremetImageWidth(imgWidth) {
-    // image
-    let image = document.querySelector("#image");
-    image.style.width = imgWidth + "px";
-}
+// function warningMessage(pixels) {
+//     if(pixels > 300) {
+//         message.textContent = "Too big. Decrease the size of the image.";
+//     } else if (pixels < 100) {
+//         message.textContent = "Too small. Increase the size of the image.";
+//     } else {
+//         message.textContent = "";
+//     }
+// }
+
+// function incrementAndDecremetImageWidth(imgWidth) {
+//     // image
+//     let image = document.querySelector("#image");
+//     image.style.width = imgWidth + "px";
+// }
+
+// function incrementBy5() {
+//     let incrementValue = pixels;
+//     if(incrementValue < 300) {
+//         pixels += 5;        
+//     }
+//     pixelsWidth.textContent = incrementValue;  
+//     incrementAndDecremetImageWidth(incrementValue);
+//     warningMessage(incrementValue);    
+// }
+
+// function decrementBy5() {
+//     if(pixels > 100) {
+//         pixels -= 5;
+//         console.log(pixels);
+
+//     }
+//     pixelsWidth.textContent = pixels;
+//     incrementAndDecremetImageWidth(pixels);
+//     warningMessage(pixels);
+// }
 
 function incrementBy5() {
-    if(pixels < 300) {
-        pixels += 5;        
+    if(pixels >= 250) {
+        message.textContent = "Too big";
+    } else {
+        pixels += 5;
+        pixelsWidth.textContent = pixels;
+        image.style.width = pixels+"px";
     }
-    pixelsWidth.textContent = pixels;  
-    incrementAndDecremetImageWidth(pixels);
-    warningMessage();    
 }
 
 function decrementBy5() {
-    if(pixels > 100) {
+    if(pixels <= 100) {
+        message.textContent = "Too small";
+    } else {
         pixels -= 5;
-        console.log(pixels);
-
+        pixelsWidth.textContent = pixels;
+        image.style.width = pixels+"px";
     }
-    pixelsWidth.textContent = pixels;
-    incrementAndDecremetImageWidth(pixels);
-    warningMessage();
 }
+
 
 incrementButton.addEventListener("click", incrementBy5);
 decrementButton.addEventListener("click", decrementBy5);
